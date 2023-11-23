3e07d06d2fe8d3dfabcb93cbce6ede4f77133d20
SebaGatti <seba.g.1391@gmail.com>
2023-11-23 11:15:21 -0300

    review dia 2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const product = require('./src/controllers/savedInDB')
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true}).then(() => {
+  product()
   server.listen(PORT, () => {
     console.log(`Server raised in port ${PORT}`); // eslint-disable-line no-console
 
