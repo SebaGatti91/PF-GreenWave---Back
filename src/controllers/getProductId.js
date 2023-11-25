312073adac76032ca564b455ac457007811f67ee
MBuenoMartinez <martin.bueno10@hotmail.com>
2023-11-25 17:56:41 -0300

    optimizacion de detail

--- src/controllers/getProductId.js
+++ src/controllers/getProductId.js
@@ -1,28 +1,19 @@
 const { Product } = require("../db");
 
 const getProductsId = async (req, res) => {
-    try {
-        let productsFromDB = await Product.findAll();
+  try {
+    const { id } = req.params;
+    const productFound = await Product.findByPk(id);
 
-        if (req.params.id) {
-            const searchId = req.params.id;
-            const foundProduct = productsFromDB.find(
-              (product) => product.id === searchId
-            );
-      
-            if (!foundProduct) {
-              return res.status(404).json({ message: "Product not found" });
-            }
-      
-            return res.status(200).json(foundProduct);
-          }
+    if (!productFound) {
+      return res.status(404).json({ message: "Product not found" });
     }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
-}
-
+    return res.status(200).json(productFound);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 module.exports = {
-    getProductsId
-}
+  getProductsId,
+};
