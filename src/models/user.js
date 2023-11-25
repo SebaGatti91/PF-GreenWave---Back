388720353176f78e24a2708f9aadb4406a9057a8
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-25 14:51:22 -0300

    Modelo `User.js` modificado, API `users.json` añadida a carpeta `apis`

--- src/models/user.js
+++ src/models/user.js
@@ -4,32 +4,36 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize..
 
 module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('User', {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue:DataTypes.UUIDV4
-     },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      credits: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-        default: 0,
-      }
-    }, {
-      timestamps: false 
-    });
-  };
+  // defino el modelo
+  sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    credits: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      default: 0,
+    }
+  }, {
+    timestamps: false
+  });
+};
 
