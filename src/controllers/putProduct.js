f85f8d0b54045cb89850f7d12761a760191e5a68
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-28 15:26:50 -0300

    Respuesta añadida a `putProduct`

--- src/controllers/putProduct.js
+++ src/controllers/putProduct.js
@@ -23,7 +23,7 @@ const putProduct = async (req, res) => {
       materials: materials || existingProduct.materials,
     });
     return res.status(200).json(modifiedProduct);
-    // return res.status(200).json({ message: 'Producto actualizado correctamente' });
+    // return res.status(200).json({ message: 'Product successfully updated' });
   } catch (error) {
     return res.status(500).send(error.message);
   }
