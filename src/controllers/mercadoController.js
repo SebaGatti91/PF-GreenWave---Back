51604e4d5284c0c63f8a0aff6b577f8f712d37cd
EDMR <emermontes15@gmail.com>
2023-11-28 21:13:12 -0500

    Mercado Pago añadido en versio 2.0.4

--- src/controllers/mercadoController.js
+++ src/controllers/mercadoController.js
@@ -1,18 +1,26 @@
-const { Router } = require("express");
-const mercadopago = require("mercadopago");
-const dotenv = require("dotenv");
-dotenv.config();
-const Mercado_Pago = Router();
+const {MercadoPagoConfig, Preference} = require("mercadopago");
+require("dotenv").config();
 
-mercadopago.configure({
-  access_token: process.env.ACCESS_TOKE || "",
-});
+const client = new MercadoPagoConfig({
+  accessToken: process.env.ACCESS_TOKEN
+ });
+
+const payment = new Preference(client)
 
 const mercadoController = async (req, res) => {
-  const producto = req.body;
+
+  // const productos = req.body;
+  // const items = productos.map((producto)=>({
+  //   title: producto.title,
+  //   unit_price: producto.price,
+  //   currency_id: "ARS",
+  //   quantity: producto.quantity,
+  // }))
 
   try {
     const preference = {
+      body:{
+      external_reference: {id :1999},
       items: [
         {
           title: "PCcc gamer",
@@ -28,11 +36,12 @@ const mercadoController = async (req, res) => {
       },
 
       auto_return: "approved",
+    }
     };
 
-    const respuesta = await mercadopago.preferences.create(preference);
+    const respuesta = await payment.create(preference);
     console.log(respuesta);
-    res.status(200).json(respuesta.response.init_point);
+    res.status(200).json(respuesta.init_point);
   } catch (error) {
     console.error(error.message);
     res.status(500).json(error.message);
