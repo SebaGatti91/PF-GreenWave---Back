23052597cd71b8f3bbeabcfeb0fcf24752ecf152
EDMR <emermontes15@gmail.com>
2023-11-29 23:00:03 -0500

    manejando la respuesta de mercadopago

--- src/controllers/getUsersById.js
+++ src/controllers/getUsersById.js
@@ -5,15 +5,21 @@ const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
     const userFound = await User.findByPk(id);
+
     const productOfUser = await Product.findAll({
       where:{ userId : id}
    }) 
+
+   const productosRelacionados = await userFound.getProducts();
+ 
   
     if (!userFound) {
       return res.status(404).json({ message: "user not found" });
     }
     return res.status(200).json({...userFound.dataValues,
-    products: productOfUser});
+    productsCreados: productOfUser,
+    productsComprados: productosRelacionados
+  });
   } catch (error) {
     return res.status(500).send(error.message);
   }
