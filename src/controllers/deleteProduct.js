237cc0fcf06825af119f94176b418c0d84c1c4a0
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-11-30 14:39:52 -0300

    `deleteProduct.js` y `putProduct.js` modificados

--- src/controllers/deleteProduct.js
+++ src/controllers/deleteProduct.js
@@ -1,20 +1,21 @@
-const { Product } = require("../db");
+const { Product } = require('../db');
 
 const deleteProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const productFound = await Product.findByPk(id);
+  try {
+    const { id } = req.params;
+    const productFound = await Product.findByPk(id);
 
-        if (!productFound) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-
-        await productFound.destroy();
-
-        return res.status(200).json({ message: 'Product successfully removed' });
-    } catch (error) {
-        return res.status(500).send(error.message);
+    if (!productFound) {
+      return res.status(404).json({ error: 'Product not found' });
     }
+
+    await productFound.update({ deleted: true });
+
+    return res.status(200).json({ message: 'Product successfully removed' });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = { deleteProduct };
+
