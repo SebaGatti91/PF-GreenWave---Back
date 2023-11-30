708646f80f72df17e5a390e9669966a0c45440b7
EDMR <emermontes15@gmail.com>
2023-11-30 13:55:35 -0500

    vamoss

--- src/controllers/responseMercado.js
+++ src/controllers/responseMercado.js
@@ -10,6 +10,7 @@ const transporter = nodemailer.createTransport({
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
+    ///este cambiooo
 });
 
 const responseMercado = async (req, res) => {
