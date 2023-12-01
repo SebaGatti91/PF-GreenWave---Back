e7bbf5d7f4c9c2351856135f158ed75db1a9fbb6
EDMR <emermontes15@gmail.com>
2023-12-01 17:28:50 -0500

    add Favoritos incluido

---
+++ src/controllers/addFavorites.js
@@ -0,0 +1,19 @@
+const { User, Product,Favorites } = require('../db');
+
+const addFavorites = async (req, res) => {
+
+    try {
+     const {userId , productId} = req.body
+     const user =  await User.findOne({where: {email: userId} })
+     const productFound = await Product.findAll({where: {id: productId}})
+
+     await user.addProduct(productFound, { through: { isFavorite: true } });
+     return res.status(200).json({ message: 'Add a favorites' });
+    }catch (error) {
+     return res.status(500).send(error.message);
+   }
+};
+
+module.exports = { 
+    addFavorites 
+};
