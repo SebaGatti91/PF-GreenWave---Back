fba301709ea46ea695a18d500d765c7404401188
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2023-12-03 20:34:37 -0300

    `getUsersById.js` modificado

--- src/controllers/getUsersById.js
+++ src/controllers/getUsersById.js
@@ -1,38 +1,40 @@
-const { User } = require("../db");
-const {Product, UserProduct }= require("../db");
+const { User, Product, UserProduct } = require("../db");
 
 const getUserById = async (req, res) => {
   try {
-    
+
     const { id } = req.params;
-    const userFound = await User.findOne({where: {email: id}});
-    
-    const productOfUser = await Product.findAll({
-      where:{ userId : userFound.id}
-   }) 
+    const userFound = await User.findOne({ where: { email: id } });
 
-   const favoriteProducts = await UserProduct.findAll({where: {
-       UserId: userFound.id,
-       isFavorite: true,
-     },
-      include: Product
-   });
+    const userProducts = await Product.findAll({
+      where: { userId: userFound.id }
+    })
 
-   const purchasedProducts = await  UserProduct.findAll({where: {
-      UserId: userFound.id,
-      isPurchase: true,
-     },
+    const favoriteProducts = await UserProduct.findAll({
+      where: {
+        UserId: userFound.id,
+        isFavorite: true,
+      },
       include: Product
     });
 
-     if (!userFound) {
-       return res.status(404).json({ message: "user not found" });
-     }
-     return res.status(200).json({...userFound.dataValues,
-     productsCreados: productOfUser,
-     productsComprados: purchasedProducts,
-     favoritos: favoriteProducts
-   }); 
+    const purchasedProducts = await UserProduct.findAll({
+      where: {
+        UserId: userFound.id,
+        isPurchase: true,
+      },
+      include: Product
+    });
+
+    if (!userFound) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({
+      ...userFound.dataValues,
+      posted: userProducts,
+      purchased: purchasedProducts,
+      favorites: favoriteProducts
+    });
 
   } catch (error) {
     return res.status(500).send(error.message);
