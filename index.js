88f0e99e5070d93be73b6177e1f8565b487a7901
Tomas Silber <tomi.silber@hotmail.com>
2023-12-05 15:27:35 -0300

    index con ejecucion de productCreate

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+const {productsCreate} = require('./src/controllers/productsCreate.js');
+
 require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const port = process.env.PORT || 3001;
 
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(port, () => {
+  server.listen(port, async() => {
+    await productsCreate();
     console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
   });
 });
